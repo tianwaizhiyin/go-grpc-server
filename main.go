105a5c5ebdebbb6846c383c1521eaa9a74d553cf
893335792@qq.com <893335792@qq.com>
2020-05-22 16:30:25 +0800

    2020-05221630

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"github.com/tianwaizhiyin/go-grpc-server.git/services"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
-	"log"
+	"io/ioutil"
 	"net"
 )
 
 func main()  {
-	creds, err := credentials.NewServerTLSFromFile("keys/server.crt", "keys/server_no_password.key")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//creds, err := credentials.NewServerTLSFromFile("keys/server.crt", "keys/server_no_password.key")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	//tls证书认证
+	cert, _ := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
+	certPool := x509.NewCertPool()
+	ca, _ := ioutil.ReadFile("cert/ca.pem")
+	certPool.AppendCertsFromPEM(ca)
+
+	creds := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert}, //服务端证书
+		ClientAuth: tls.RequireAndVerifyClientCert,
+		ClientCAs: certPool,
+	})
+
+
 	rpcServer := grpc.NewServer(grpc.Creds(creds))
 	services.RegisterProdServiceServer(rpcServer, new(services.ProdService))
 
 	list, _ := net.Listen("tcp", ":8080")
 	rpcServer.Serve(list)
-}
 
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Println(request)
+	//	rpcServer.ServeHTTP(writer,request)
+	//})
+	//httpServer := &http.Server{
+	//	Addr:":8080",
+	//	Handler:mux,
+	//}
+	//httpServer.ListenAndServeTLS("keys/server.crt", "keys/server_no_password.key")
+
+}
