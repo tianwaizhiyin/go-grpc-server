5021b265bfca6305cd701e95dfdf4263f7648172
893335792@qq.com <893335792@qq.com>
2020-05-22 18:11:51 +0800

    2020-05221811

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ replace goTest => D:\golang\gopath\src\go-grpc-server
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/grpc-ecosystem/grpc-gateway v1.14.5
 	golang.org/x/net v0.0.0-20200520182314-0ba52f642ac2 // indirect
 	golang.org/x/sys v0.0.0-20200519105757-fe76b779f299 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20200519141106-08726f379972 // indirect
+	google.golang.org/genproto v0.0.0-20200521103424-e9a78aa275b7
 	google.golang.org/grpc v1.29.1
 	google.golang.org/protobuf v1.23.0
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"crypto/tls"
-	"crypto/x509"
+	"github.com/tianwaizhiyin/go-grpc-server.git/helper"
 	"github.com/tianwaizhiyin/go-grpc-server.git/services"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-	"io/ioutil"
 	"net"
 )
 
@@ -17,22 +14,22 @@ func main()  {
 	//}
 
 	//tls证书认证
-	cert, _ := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
-	certPool := x509.NewCertPool()
-	ca, _ := ioutil.ReadFile("cert/ca.pem")
-	certPool.AppendCertsFromPEM(ca)
+	//cert, _ := tls.LoadX509KeyPair("cert/server.pem", "cert/server.key")
+	//certPool := x509.NewCertPool()
+	//ca, _ := ioutil.ReadFile("cert/ca.pem")
+	//certPool.AppendCertsFromPEM(ca)
+	//
+	//creds := credentials.NewTLS(&tls.Config{
+	//	Certificates: []tls.Certificate{cert}, //服务端证书
+	//	ClientAuth: tls.RequireAndVerifyClientCert,
+	//	ClientCAs: certPool,
+	//})
 
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{cert}, //服务端证书
-		ClientAuth: tls.RequireAndVerifyClientCert,
-		ClientCAs: certPool,
-	})
 
-
-	rpcServer := grpc.NewServer(grpc.Creds(creds))
+	rpcServer := grpc.NewServer(grpc.Creds(helper.GetServerCreds()))
 	services.RegisterProdServiceServer(rpcServer, new(services.ProdService))
 
-	list, _ := net.Listen("tcp", ":8080")
+	list, _ := net.Listen("tcp", ":8081")
 	rpcServer.Serve(list)
 
 	//mux := http.NewServeMux()
@@ -45,5 +42,4 @@ func main()  {
 	//	Handler:mux,
 	//}
 	//httpServer.ListenAndServeTLS("keys/server.crt", "keys/server_no_password.key")
-
 }
