466affb33e05a1bd68f2092face6cefef7af2402
893335792@qq.com <893335792@qq.com>
2020-05-25 10:37:13 +0800

    2020-0525-1037

--- services/ProdService.go
+++ services/ProdService.go
@@ -8,6 +8,34 @@ type ProdService struct {
 
 }
 
+func (this *ProdService) GetProdSInfo(context.Context, *ProdRequest) (*ProdModel, error)  {
+	ret := ProdModel{ProdId:101,
+		ProdName:"测试商品",
+		ProdPrice:20.5,
+	}
+	return &ret, nil
+}
+
 func (this *ProdService) GetProdStock(ctx context.Context, request *ProdRequest) (*ProdResponse, error)  {
-	return &ProdResponse{ProdStock: 28}, nil
+	var stock int32=0;
+	if request.ProdArea==ProdAreas_A {
+		stock=30
+	}else if request.ProdArea==ProdAreas_B {
+		stock=31
+	}else {
+		stock=50
+	}
+	return &ProdResponse{ProdStock: stock}, nil
+}
+
+func (this *ProdService) GetProdStocks(ctx context.Context, size *QuerySize) (*ProdResponseList, error)  {
+	Prodres := []*ProdResponse {
+		&ProdResponse{ProdStock:28},
+		&ProdResponse{ProdStock:29},
+		&ProdResponse{ProdStock:30},
+		&ProdResponse{ProdStock:31},
+	}
+	return &ProdResponseList{
+		Prodres:Prodres,
+	}, nil
 }
